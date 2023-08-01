00eb03ba1431b974fdd92292af650589d596d810
Whitley Beers <whitleybeers@gmail.com>
2023-07-31 21:09:15 -0500

    initial register form done

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -6,9 +6,22 @@ import { registerUser } from '../utils/auth'; // Update with path to registerUse
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    firstName: '',
+    lastName: '',
+    email: user.fbUser.email,
     uid: user.uid,
+    profileImageUrl: '',
+    address: '',
+    phoneNumber: '',
   });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,11 +30,41 @@ function RegisterForm({ user, updateUser }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+      {/* FIRST NAME FIELD */}
+      <Form.Group className="mb-3" controlId="firstName">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control as="text" name="firstName" required value={formData.firstName} onChange={handleChange} />
+        <Form.Text className="text-muted" />
       </Form.Group>
+
+      {/* LAST NAME FIELD */}
+      <Form.Group className="mb-3" controlId="lastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control as="text" name="lastName" required value={formData.lastName} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+
+      {/* PROFILE IMAGE URL FIELD */}
+      <Form.Group className="mb-3" controlId="profileImageUrl">
+        <Form.Label>Profile Image</Form.Label>
+        <Form.Control type="url" name="profileImageUrl" required value={formData.profileImageUrl} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+
+      {/* ADDRESS FIELD */}
+      <Form.Group className="mb-3" controlId="address">
+        <Form.Label>Enter your address</Form.Label>
+        <Form.Control type="textarea" name="address" required value={formData.address} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+
+      {/* PHONE NUMBER FIELD */}
+      <Form.Group className="mb-3" controlId="phoneNumber">
+        <Form.Label>Enter your phone number</Form.Label>
+        <Form.Control type="number" name="phoneNumber" pattern="^\d{3}-\d{3}-\d{4}$" required value={formData.phoneNumber} onChange={handleChange} />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
@@ -32,6 +75,10 @@ function RegisterForm({ user, updateUser }) {
 RegisterForm.propTypes = {
   user: PropTypes.shape({
     uid: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    fbUser: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+    }),
   }).isRequired,
   updateUser: PropTypes.func.isRequired,
 };
